f79ea06ce6f7b6ef3d5c0bd49ff217d3cf3309a2
patrickkivuvo <patrickkivuvo@gmail.com>
2023-03-21 17:08:26 +0000

    updated some files

--- backend/Software/src/main/java/com/login/Software/model/User.java
+++ backend/Software/src/main/java/com/login/Software/model/User.java
@@ -7,6 +7,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Map;
+
 @Data
 @AllArgsConstructor
 @Document(collection = "user")
